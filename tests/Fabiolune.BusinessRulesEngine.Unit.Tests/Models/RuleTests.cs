6880690ddfd72a722ff9e3647818604aa3ae8ed2
fabio.lonegro <fabio.lonegro@deltatre.com>
2020-09-22 17:25:02 +0200

    added test on ToString

---
+++ tests/Fabiolune.BusinessRulesEngine.Unit.Tests/Models/RuleTests.cs
@@ -0,0 +1,24 @@
+﻿using Fabiolune.BusinessRulesEngine.Internals;
+using Fabiolune.BusinessRulesEngine.Models;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Fabiolune.BusinessRulesEngine.Unit.Tests.Models
+{
+    public class RuleTests
+    {
+        [Test]
+        public void ToString_ShouldReturnJsonSerialization()
+        {
+            var rule = new Rule("property", OperatorType.Contains, "value");
+
+            var result = rule.ToString();
+
+            result
+                .Replace("\n", "")
+                .Replace("\r", "")
+                .Should()
+                .Be("{  \"Property\": \"property\",  \"Operator\": 7,  \"Value\": \"value\",  \"Description\": null,  \"Code\": null}");
+        }
+    }
+}
