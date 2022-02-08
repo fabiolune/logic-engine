4e1d7b8af0a8786af7bb73687ca5163474a1ac24
fabiolune <fabiolune@gmail.com>
2022-02-08 23:52:31 +0100

    removed useless extension

--- src/RulesEngine/Extensions/EnumerableExtensions.cs
+++ src/RulesEngine/Extensions/EnumerableExtensions.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics.Contracts;
+﻿using System.Collections.Generic;
 using RulesEngine.Interfaces;
 
 namespace RulesEngine.Extensions
@@ -9,19 +7,5 @@ namespace RulesEngine.Extensions
     {
         public static IEnumerable<T> Filter<T>(this IEnumerable<T> @this, IRulesManager<T> manager) where T : new() => manager.Filter(@this);
         public static T FirstOrDefault<T>(this IEnumerable<T> @this, IRulesManager<T> manager) where T : new() => manager.FirstOrDefault(@this);
-
-        [Pure]
-        public static TS FoldWhile<TS, T>(this IEnumerable<T> list, TS state, Func<TS, T, TS> folder, Func<T, bool> predItem)
-        {
-            foreach (var item in list)
-            {
-                if (!predItem(item))
-                {
-                    return state;
-                }
-                state = folder(state, item);
-            }
-            return state;
-        }
     }
 }
