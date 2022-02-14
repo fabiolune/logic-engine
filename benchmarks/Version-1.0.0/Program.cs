7989acb2b313b6e0bac60efbe1936c4a7a9ded4a
fabio.lonegro <fabio.lonegro@deltatre.com>
2022-02-14 12:05:55 +0100

    added benchmark project for latest stable version

---
+++ benchmarks/Version-1.0.0/Program.cs
@@ -0,0 +1,113 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Running;
+using LogicEngine;
+using LogicEngine.Internals;
+using LogicEngine.Models;
+using Serilog.Core;
+
+internal class Data
+{
+    internal struct TestModel
+    {
+        public string StringProperty { get; set; }
+    }
+    internal static RulesCatalog Catalog =>
+    new()
+    {
+        RulesSets = new List<RulesSet>
+                {
+                    new()
+                    {
+                        Rules = new List<Rule>
+                        {
+                            new(nameof(TestModel.StringProperty), OperatorType.Equal, "wrong"),
+                            new(nameof(TestModel.StringProperty), OperatorType.Equal, "wrong"),
+                            new(nameof(TestModel.StringProperty), OperatorType.Equal, "wrong"),
+                            new(nameof(TestModel.StringProperty), OperatorType.Equal, "wrong"),
+                            new(nameof(TestModel.StringProperty), OperatorType.Equal, "wrong"),
+                            new(nameof(TestModel.StringProperty), OperatorType.Equal, "wrong"),
+                            new(nameof(TestModel.StringProperty), OperatorType.Equal, "wrong"),
+                            new(nameof(TestModel.StringProperty), OperatorType.Equal, "wrong"),
+                            new(nameof(TestModel.StringProperty), OperatorType.Equal, "wrong"),
+                            new(nameof(TestModel.StringProperty), OperatorType.Equal, "wrong"),
+                            new(nameof(TestModel.StringProperty), OperatorType.Equal, "wrong"),
+                            new(nameof(TestModel.StringProperty), OperatorType.Equal, "wrong"),
+                            new(nameof(TestModel.StringProperty), OperatorType.Equal, "wrong"),
+                            new(nameof(TestModel.StringProperty), OperatorType.Equal, "wrong"),
+                            new(nameof(TestModel.StringProperty), OperatorType.Equal, "wrong"),
+                            new(nameof(TestModel.StringProperty), OperatorType.Equal, "wrong"),
+                            new(nameof(TestModel.StringProperty), OperatorType.Equal, "wrong"),
+                            new(nameof(TestModel.StringProperty), OperatorType.Equal, "wrong"),
+                            new(nameof(TestModel.StringProperty), OperatorType.Equal, "wrong"),
+                            new(nameof(TestModel.StringProperty), OperatorType.Equal, "wrong"),
+                            new(nameof(TestModel.StringProperty), OperatorType.Equal, "wrong"),
+                        }
+                    },
+                    new()
+                    {
+                        Rules = new List<Rule>
+                        {
+                            new(nameof(TestModel.StringProperty), OperatorType.Equal, "wrong"),
+                            new(nameof(TestModel.StringProperty), OperatorType.Equal, "wrong"),
+                            new(nameof(TestModel.StringProperty), OperatorType.Equal, "wrong"),
+                            new(nameof(TestModel.StringProperty), OperatorType.Equal, "wrong"),
+                            new(nameof(TestModel.StringProperty), OperatorType.Equal, "wrong"),
+                            new(nameof(TestModel.StringProperty), OperatorType.Equal, "wrong"),
+                            new(nameof(TestModel.StringProperty), OperatorType.Equal, "wrong"),
+                            new(nameof(TestModel.StringProperty), OperatorType.Equal, "wrong"),
+                            new(nameof(TestModel.StringProperty), OperatorType.Equal, "wrong"),
+                            new(nameof(TestModel.StringProperty), OperatorType.Equal, "wrong"),
+                            new(nameof(TestModel.StringProperty), OperatorType.Equal, "wrong"),
+                            new(nameof(TestModel.StringProperty), OperatorType.Equal, "wrong"),
+                            new(nameof(TestModel.StringProperty), OperatorType.Equal, "wrong"),
+                            new(nameof(TestModel.StringProperty), OperatorType.Equal, "wrong"),
+                            new(nameof(TestModel.StringProperty), OperatorType.Equal, "wrong"),
+                            new(nameof(TestModel.StringProperty), OperatorType.Equal, "wrong"),
+                            new(nameof(TestModel.StringProperty), OperatorType.Equal, "wrong"),
+                            new(nameof(TestModel.StringProperty), OperatorType.Equal, "wrong"),
+                            new(nameof(TestModel.StringProperty), OperatorType.Equal, "wrong"),
+                            new(nameof(TestModel.StringProperty), OperatorType.Equal, "wrong"),
+                            new(nameof(TestModel.StringProperty), OperatorType.Equal, "wrong"),
+                            new(nameof(TestModel.StringProperty), OperatorType.Equal, "correct"),
+                        }
+                    }
+                }
+    };
+}
+
+[MemoryDiagnoser]
+[MarkdownExporterAttribute.GitHub]
+[JsonExporterAttribute.Brief]
+public class PreviousImplementationBenchmarks
+{
+    private readonly RulesManager<Data.TestModel> _manager = new(new RulesCompiler(Logger.None));
+    private readonly RulesManager<Data.TestModel> _manager2 = new(new RulesCompiler(Logger.None));
+
+    private readonly Data.TestModel _item = new()
+    {
+        StringProperty = "correct"
+    };
+
+    public PreviousImplementationBenchmarks()
+    {
+        _manager.SetCatalog(Data.Catalog);
+    }
+
+    [Benchmark]
+    public void SetCatalog() => _manager2.SetCatalog(Data.Catalog);
+
+    [Benchmark]
+    public void RulesApplication() => _manager.ItemSatisfiesRules(_item);
+
+    [Benchmark]
+    public void RulesApplicationWithMessage() => _manager.ItemSatisfiesRulesWithMessage(_item);
+}
+
+internal static class Program
+{
+    internal static void Main()
+    {
+        BenchmarkRunner.Run<PreviousImplementationBenchmarks>(DefaultConfig.Instance.WithOption(ConfigOptions.DisableOptimizationsValidator, true));
+    }
+}
