f3eb7110561f8486ba824d248b9ca5897b3fb4c8
fabiolune <fabiolune@gmail.com>
2023-05-24 17:19:52 +0200

    added new ICompositeRulesSetManager with tests and implementation

---
+++ src/LogicEngine/Interfaces/ICompositeRulesSetManager.cs
@@ -0,0 +1,11 @@
+﻿using LogicEngine.Models;
+using System.Collections.Generic;
+using TinyFp;
+
+namespace LogicEngine.Interfaces;
+
+public interface ICompositeRulesSetManager<in T, TKey> where T : new()
+{
+    IEnumerable<(TKey,RulesSet)> Set { set; }
+    Option<TKey> FirstMatching(T item);
+}
