68374fa1c05f647a9ec94c924562a652c3f13ab6
fabiolune <fabiolune@gmail.com>
2023-06-20 14:26:03 +0200

    fixed some code smells and improved performances

--- benchmarks/Current/Program.cs
+++ benchmarks/Current/Program.cs
@@ -5,7 +5,15 @@ namespace Current;
 
 internal static class Program
 {
-    internal static void Main() =>
+    internal static void Main()
+    {
+        //BenchmarkRunner
+        //    .Run<CompileBenchmarks>(DefaultConfig.Instance.WithOption(ConfigOptions.DisableOptimizationsValidator, true));
+
         BenchmarkRunner
-            .Run<CurrentImplementationBenchmarks>(DefaultConfig.Instance.WithOption(ConfigOptions.DisableOptimizationsValidator, true));
+            .Run<ApplyBenchmarks>(DefaultConfig.Instance.WithOption(ConfigOptions.DisableOptimizationsValidator, true));
+
+        //BenchmarkRunner
+        //    .Run<DetailedApplyBenchmarks>(DefaultConfig.Instance.WithOption(ConfigOptions.DisableOptimizationsValidator, true));
+    }
 }
